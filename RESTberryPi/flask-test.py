61d26b1b93d907248b28be8193659fb62ea9b593
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-17 14:34:28 +0200

    Add rx to flask-test

--- RESTberryPi/flask-test.py
+++ RESTberryPi/flask-test.py
@@ -1,15 +1,30 @@
 from flask import Flask, request
+from rx import subjects
 
 app = Flask(__name__)
+
+subject = subjects.Subject()
+
+
+@app.route('/', methods=['GET'])
+def send_start_page():
+    return "use /api/gpio?pin=1&state=HIGH"
 
 
 @app.route('/api/gpio', methods=['GET'])
 def set_gpio():
     pin = request.args.get('pin')
-    value = request.args.get('value')
-    print("pin " + pin + " set to " + value)
-    return "pin " + pin + " set to " + value
+    state = request.args.get('state')
+    subject.on_next({'pin': pin, 'state': state})
+    return "received and put in queue: pin {}, state {}".format(pin, state)
+
+
+def pin_handler(value):
+    pin = value['pin']
+    state = value['state']
+    print("pin " + pin + " set to " + state)
 
 
 if __name__ == '__main__':
+    subscription = subject.subscribe(on_next=lambda x: pin_handler(x))
     app.run(debug=True)
