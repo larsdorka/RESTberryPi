d91baae4faa198d45d469a0a5dc3d1a55135fb29
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-19 10:27:18 +0200

    Add usage of webinterface and iohandler in main application

--- RESTberryPi/RESTberryPi.py
+++ RESTberryPi/RESTberryPi.py
@@ -1,16 +1,33 @@
 import time
 
 import ioHandler
+import webInterface
+
+
+def gpio_write_request_handler(io, value):
+    channel = int(value['channel'])
+    string_state = value['state']
+    string_state.upper()
+    bool_state = False
+    if state in ['ON', 'HIGH', '1']:
+        bool_state = True
+    elif state in ['OFF', 'LOW', '0']:
+        bool_state = False
+    io.set_channel(channel, bool_state)
+
 
 # main application loop
 if __name__ == '__main__':
-    ioHandler = ioHandler.IOHandler()
-    ioHandler.setup_pins()
+    webInterface.run()
+    io = ioHandler.IOHandler()
+    io.setup_pins()
+    subscription = webInterface.gpio_write_request.subscribe(
+        on_next=lambda x: gpio_write_request_handler(io, x))
     counter = 0
     while True:
         time.sleep(0.05)
-        for index in range(len(ioHandler.output_channels)):
+        for index in range(len(io.output_channels)):
             state = (counter & (2 ** index)) == 1
-            ioHandler.set_channel(index, state)
+            io.set_channel(index, state)
         counter += 1
         counter %= 256
