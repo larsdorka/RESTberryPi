f854692d7137255474a5e5ca7df2ed05b22cd1f2
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-19 10:13:22 +0200

    Add simple flask webinterface as module

---
+++ RESTberryPi/webInterface.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from rx import subjects
+
+app = Flask(__name__)
+gpio_write_request = subjects.Subject()
+
+
+@app.route('/', methods=['GET'])
+def send_index_page():
+    return "API is running"
+
+
+@app.route('/api/gpioWrite/channel/<channel>/state/<state>', methods=['GET'])
+def gpio_write(channel, state):
+    gpio_write_request.on_next({'pin': channel, 'state': state})
+    return "received and put in queue: channel {}, state {}".format(channel, state)
+
+
+@app.route('/api/gpioWrite', methods=['GET'])
+def gpio_write_with_params():
+    channel = request.args.get('channel')
+    state = request.args.get('state')
+    gpio_write_request.on_next({'channel': channel, 'state': state})
+    return "received and put in queue: channel {}, state {}".format(channel, state)
+
+
+def run():
+    app.run(debug=True, host='0.0.0.0')
