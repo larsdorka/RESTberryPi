3bb9f52a65c360fb9ab45315c1fa197d21c5c671
larsdorka <lars.dorka@googlemail.com>
2017-07-19 18:13:44 +0200

    Add jquery functions to send get requests on channel buttons

--- RESTberryPi/static/js/main.js
+++ RESTberryPi/static/js/main.js
@@ -135,23 +135,6 @@ $(function () {
     $.ajaxSetup({"contentType": "text/json"});
 });
 
-$(function () {
-    $("#btn_send").click(
-        function () {
-            var myObj = {};
-            var myColor = {};
-            var myText = {};
-            myColor["content"] = $("#colorContentInput").val().toUpperCase();
-            myColor["mode"] = $("#colorModeSelector").val();
-            myText["content"] = $("#textContentInput").val();
-            myText["mode"] = $("#textModeSelector").val();
-            myObj["color"] = myColor;
-            myObj["text"] = myText;
-            var myJson = JSON.stringify(myObj);
-            $.post("api/setdisplay", myJson, function () {
-            });
-        });
-});
 
 $(function () {
     $("#btn_clear").click(
@@ -163,10 +146,20 @@ $(function () {
 });
 
 $(function () {
-    $("#btn_setled").click(
-        function () {
-            var params = "?chan=" + $("#channelSelect").val() + "&val=" + $("#valueInput").val();
-            $.get("api/led" + params, function () {
-            });
-        });
+    $("[name=btn_chan]").click(gpioWrite_query);
 });
+
+function gpioWrite_query() {
+    console.log($(this).attr('class'));
+    const state = $(this).hasClass('btn-success');
+    if (state) {
+        $(this).removeClass('btn-success');
+        $(this).addClass('btn-danger');
+    } else {
+        $(this).removeClass('btn-danger');
+        $(this).addClass('btn-success');
+    }
+    var params = "?channel=" + $(this).text().trim() + "&state=" + state.toString();
+    $.get("api/gpioWrite" + params, function () {
+    });
+};
