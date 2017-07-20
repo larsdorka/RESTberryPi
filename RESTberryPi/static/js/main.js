cf49ca36618ebedefda872fd00bbd21d8b3b7d0c
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-20 13:32:46 +0200

    Fix boolean state in main.js again

--- RESTberryPi/static/js/main.js
+++ RESTberryPi/static/js/main.js
@@ -35,15 +35,15 @@ $(function () {
 
 function gpioWrite_query() {
     console.log($(this).attr('class'));
-    const state = $(this).hasClass('btn-success');
-    if (state) {
+    const state = $(this).hasClass('btn-danger');
+    if (!state) {
         $(this).removeClass('btn-success');
         $(this).addClass('btn-danger');
     } else {
         $(this).removeClass('btn-danger');
         $(this).addClass('btn-success');
     }
-    var params = "?channel=" + $(this).text().trim() + "&state=" + !state.toString();
+    var params = "?channel=" + $(this).text().trim() + "&state=" + state.toString();
     $.get("api/gpioWrite" + params, function () {
     });
 };
