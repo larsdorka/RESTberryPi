c07a57af6132c15108dbe5d4736ab3f84ec421b2
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-20 12:01:21 +0200

    Remove I/O hardware test from main application

--- RESTberryPi/RESTberryPi.py
+++ RESTberryPi/RESTberryPi.py
@@ -1,20 +1,8 @@
-import time
-
 import ioHandler
 import webInterface
 
-
 # main application loop
 if __name__ == '__main__':
-    webInterface.run()
     io = ioHandler.IOHandler()
     io.setup_pins(webInterface.gpio_write_request)
-
-    counter = 0
-    while True:
-        time.sleep(0.05)
-        for index in range(len(io.output_channels)):
-            state = (counter & (2 ** index)) == 1
-            io.set_channel(index, state)
-        counter += 1
-        counter %= 256
+    webInterface.run()
