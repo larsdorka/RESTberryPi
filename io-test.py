1b18e09f772736ccd6403b549b6765a281c75ff9
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-20 12:01:03 +0200

    Add simple I/O hardware test

---
+++ io-test.py
@@ -0,0 +1,16 @@
+import time
+
+import ioHandler
+
+# main application loop
+if __name__ == '__main__':
+    io = ioHandler.IOHandler()
+
+    counter = 0
+    while True:
+        time.sleep(0.05)
+        for index in range(len(io.output_channels)):
+            state = (counter & (2 ** index)) == 1
+            io.set_channel(index, state)
+        counter += 1
+        counter %= 256
